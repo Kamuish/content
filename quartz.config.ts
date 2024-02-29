cf16be75c6706ce57929a0ee64bdd9fabb9019d8
Kamuish <andremiguel952@gmail.com>
2024-02-29 17:59:05 +0000

    updating website

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,8 +17,8 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    ignorePatterns: ["private", "templates", ".obsidian", "venv"],
+    defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
